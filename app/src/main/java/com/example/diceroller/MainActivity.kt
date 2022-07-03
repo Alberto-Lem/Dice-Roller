7a9a737acf0b918f18c81634b938f9646995127a
Alberto Lem <lemxona72.1@gmail.com>
2022-07-02 21:39:31 -0600

    Empezando a retomar kotlin

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +18,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Tira los dados y actualiza la pantalla con el resultado.
     private fun rollDice() {
+        //Crea un nuevo objeto dado con 6 lados y tira los dados
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView:TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage:ImageView = findViewById(R.id.imageView)
+
+        /*when (diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }*/
+        //Determina que ID de recurso(imagen) dibujable usa la funcion de tirados de dados
+        val drawableResource = when (diceRoll){
+            1 ->R.drawable.dice_1
+            2 ->R.drawable.dice_2
+            3 ->R.drawable.dice_3
+            4 ->R.drawable.dice_4
+            5 ->R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+        //Actualiza ImageWiev con el ID de recurso(Imagen) dibujable correcto
+        diceImage.setImageResource(drawableResource)
+        //Actualiza la descripcion de contenido
+        diceImage.contentDescription = diceRoll.toString()
+        /*val myFirstDice =Dice (6)
+        val rollResult = myFirstDice.roll()
+        val numero_suerte = 4
+
+        when (rollResult){
+             numero_suerte -> println("¡Ganaste!")
+            1 -> println("¡lo siento mucho!, Sacaste un 1. ¡vuelve a intentarlo!")
+            2 -> println("lamentablemente, Sacaste un 2. ¡intente de nuevo!")
+            2 -> println("Desafortunadamente,  Sacaste un 3. ¡vuelve a intentarlo!")
+            2 -> println("¡No llores!, Sacaste un 5. ¡intente de nuevo!")
+            2 -> println("¡Disculpas!, Sacaste un 6. ¡vuelve a intentarlo!")
+        }*/
     }
 }
 class Dice(private val numSides: Int){
